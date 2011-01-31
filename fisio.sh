be0891c5150b383ecef4d8548c6e63bc68836357
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-01-31 17:23:22 -0200

    primeira versao

---
+++ fisio.sh
@@ -0,0 +1,12 @@
+sleep $1 && say "start" && sleep $2 && say "done"
+sleep $1 && say "start" && sleep $2 && say "done"
+sleep $1 && say "start" && sleep $2 && say "done"
+sleep $1 && say "start" && sleep $2 && say "done"
+sleep $1 && say "start" && sleep $2 && say "done"
+say "5 so far" && sleep 5
+sleep $1 && say "start" && sleep $2 && say "done"
+sleep $1 && say "start" && sleep $2 && say "done"
+sleep $1 && say "start" && sleep $2 && say "done"
+sleep $1 && say "start" && sleep $2 && say "done"
+sleep $1 && say "start" && sleep $2 && say "done"
+say "finished"
