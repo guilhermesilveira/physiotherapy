cd343ea9a6dda3e3f64082cb67f01d3f8103a685
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-01-31 17:25:34 -0200

    for for the win

--- fisio.sh
+++ fisio.sh
@@ -1,8 +1,6 @@
-sleep $1 && say "start" && sleep $2 && say "done"
-sleep $1 && say "start" && sleep $2 && say "done"
-sleep $1 && say "start" && sleep $2 && say "done"
-sleep $1 && say "start" && sleep $2 && say "done"
-sleep $1 && say "start" && sleep $2 && say "done"
+for i in `jot - 1 5`; do
+  sleep $1 && say "start" && sleep $2 && say "done"
+done
 say "5 so far" && sleep 5
 sleep $1 && say "start" && sleep $2 && say "done"
 sleep $1 && say "start" && sleep $2 && say "done"
