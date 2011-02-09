2829796442bf5e2e4fd18daf5504fcb55bbc742c
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-02-09 20:45:42 -0200

    fixing some exercises

--- ab.sh
+++ ab.sh
@@ -4,5 +4,7 @@ say "hold"
 ./fisio.sh 10 10
 say "hold and bridge"
 ./fisio.sh 9 11
+say "one leg at a time. good luck!"
+./fisio.sh 17 12
 say "turn around. time for your back"
 ./fisio.sh 16 11
--- knees.sh
+++ knees.sh
@@ -1,2 +1,4 @@
 say "knees position"
 ./fisio.sh 8 20
+say "knees on top of the table"
+./fisio.sh 10 20
